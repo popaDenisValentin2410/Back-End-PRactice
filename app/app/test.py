45299c00f4c0babacadf384bdc468b82941bf7bc
Popa Denis-Valentin <alu0101532295@ull.edu.es>
2023-02-23 18:08:42 +0200

    Git Changes

---
+++ app/app/test.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """Test for calc"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 2)
+        self.assertEqual(res, 7)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
